3f62d3c820cb79aa83b33be2ae51352ffdd265bd
Augustin Kavera <augustin.kavera@gmail.com>
2018-06-04 14:47:17 +0200

    Add : New views general with his controller

--- src/AppBundle/Controller/GeneralViewsController.php
+++ src/AppBundle/Controller/GeneralViewsController.php
@@ -9,7 +9,52 @@
 namespace AppBundle\Controller;
 
 
-class GeneralViewsController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class GeneralViewsController extends Controller
 {
+
+    /**
+     * @Method({"GET"})
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/gts", name="gts")
+     */
+    public function generalTermsAction()
+    {
+        return $this->render('general/gts.html.twig');
+    }
+
+    /**
+     * @Method({"GET"})
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/practical-informations", name="practical_informations")
+     */
+    public function newAction()
+    {
+        return $this->render('general/pratical-infos.html.twig');
+    }
+
+    /**
+     * @Method({"GET"})
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/legal-mentions", name="legal_mentions")
+     */
+    public function legalMentionsAction()
+    {
+        return $this->render('general/legal-mentions.html.twig');
+    }
+
+    /**
+     * @Method({"GET"})
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/contact-us", name="contact_us")
+     */
+    public function contactAction()
+    {
+        return $this->render('general/contact.html.twig');
+    }
 
 }
